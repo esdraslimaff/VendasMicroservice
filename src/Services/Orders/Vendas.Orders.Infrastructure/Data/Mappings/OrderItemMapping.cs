66c17fe90ba903b6d76b3bac7d6c704a544620e3
Esdras Lima <esdraslimaf@gmail.com>
2026-01-29 00:05:36 -0300

    feat: Adiciona mapeamento para OrderItem para ser usado no Context Factory

---
+++ src/Services/Orders/Vendas.Orders.Infrastructure/Data/Mappings/OrderItemMapping.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vendas.Orders.Domain.Entities;
+
+namespace Vendas.Orders.Infrastructure.Data.Mappings
+{
+    public class OrderItemMapping : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.ToTable("OrderItems");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.ProductId)
+                .IsRequired();
+
+            builder.Property(x => x.ProductName)
+                .HasMaxLength(250)
+                .IsRequired();
+
+            builder.Property(x => x.UnitPrice)
+                .HasColumnType("decimal(18,2)")
+                .IsRequired();
+
+            builder.Property(x => x.Quantity)
+                .IsRequired();
+        }
+    }
+}
