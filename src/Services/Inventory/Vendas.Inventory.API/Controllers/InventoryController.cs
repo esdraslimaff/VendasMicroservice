59220c5f4db1938cd619905aeae0f926f14ca38b
Esdras Lima <esdraslimaf@gmail.com>
2026-01-28 21:29:51 -0300

    Adicionando controllers em ambos microservicos

---
+++ src/Services/Inventory/Vendas.Inventory.API/Controllers/InventoryController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vendas.Inventory.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { Message = "API Orders funcionando!" });
+        }
+    }
+}
